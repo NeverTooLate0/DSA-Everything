9f53ab7d076b5c3ea312af5eec7da181cfc26b30
NeverTooLate <azzamxh@gmail.com>
2025-03-26 14:29:15 +0530

    Update circular-doubly.c

--- Linked-List/circular-doubly.c
+++ Linked-List/circular-doubly.c
@@ -1,1 +1,211 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define newnode (struct node *)malloc(sizeof(struct node))
+
+struct node
+{
+     struct node *prev;
+    int data;
+    struct node *next;
+};
+
+struct node *create(struct node *H, int n)
+{
+    struct node *s;
+    int i;
+    H = newnode;
+    printf("Enter data: ");
+    scanf("%d", &H->data);
+    H->next = H;
+    H->prev = H;
+    s = H;
+    for (i = 1; i < n; i++)
+    {
+        struct node *t = newnode;
+        printf("Enter data: ");
+        scanf("%d", &t->data);
+        t->next = H;
+        t->prev = s;
+        s->next = t;
+        H->prev = t;
+        s = t;
+    }
+    return H;
+}
+
+void display(struct node *H)
+{
+    struct node *s = H;
+    if (!H)
+        return;
+    printf("Linked list: ");
+    do
+    {
+        printf("%d ", s->data);
+        s = s->next;
+    } while (s != H);
+    printf("\n");
+}
+
+int length(struct node *H)
+{
+    struct node *s = H;
+    int l = 0;
+    if (!H)
+        return 0;
+    do
+    {
+        l++;
+        s = s->next;
+    } while (s != H);
+    return l;
+}
+
+struct node *insert(struct node *H, int x, int pos)
+{
+    struct node *t = newnode;
+    t->data = x;
+    if (!H)
+    {
+        t->next = t->prev = t;
+        return t;
+    }
+    int l = length(H);
+    struct node *s = H;
+    if (pos == 1)
+    {
+        t->next = H;
+        t->prev = H->prev;
+        H->prev->next = t;
+        H->prev = t;
+        H = t;
+    }
+    else
+    {
+        for (int i = 1; i < pos - 1; i++)
+            s = s->next;
+        t->next = s->next;
+        t->prev = s;
+        s->next->prev = t;
+        s->next = t;
+    }
+    return H;
+}
+
+struct node *deleteNode(struct node *H, int pos)
+{
+    if (!H)
+        return NULL;
+    struct node *s = H;
+    int l = length(H);
+    if (pos == 1)
+    {
+        if (H->next == H)
+        {
+            free(H);
+            return NULL;
+        }
+        H->prev->next = H->next;
+        H->next->prev = H->prev;
+        struct node *t = H;
+        H = H->next;
+        free(t);
+    }
+    else
+    {
+        for (int i = 1; i < pos; i++)
+            s = s->next;
+        s->prev->next = s->next;
+        s->next->prev = s->prev;
+        free(s);
+    }
+    return H;
+}
+
+struct node *erase(struct node *H)
+{
+    if (!H)
+        return NULL;
+    struct node *s = H->next;
+    while (s != H)
+    {
+        struct node *t = s;
+        s = s->next;
+        free(t);
+    }
+    free(H);
+    return NULL;
+}
+
+int main()
+{
+    int n, l, x, pos, ch;
+    struct node *H = NULL;
+    while (1)
+    {
+        printf("Menu\n");
+        printf("1. Create\n");
+        printf("2. Display\n");
+        printf("3. Length\n");
+        printf("4. Insert\n");
+        printf("5. Delete\n");
+        printf("6. Erase\n");
+        printf("7. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &ch);
+        switch (ch)
+        {
+        case 1:
+            printf("How many nodes in list: ");
+            scanf("%d", &n);
+            H = create(H, n);
+            break;
+        case 2:
+            display(H);
+            break;
+        case 3:
+            l = length(H);
+            printf("Length of list: %d\n", l);
+            break;
+        case 4:
+            printf("Enter the position to insert the data: ");
+            scanf("%d", &pos);
+            l = length(H);
+            if (pos < 1 || pos > l + 1)
+            {
+                printf("Invalid Position!\n");
+            }
+            else
+            {
+                printf("Enter data: ");
+                scanf("%d", &x);
+                H = insert(H, x, pos);
+            }
+            break;
+        case 5:
+            printf("Enter position to delete data: ");
+            scanf("%d", &pos);
+            l = length(H);
+            if (pos < 1 || pos > l)
+            {
+                printf("Invalid Position!\n");
+            }
+            else
+            {
+                H = deleteNode(H, pos);
+            }
+            break;
+        case 6:
+            H = erase(H);
+            printf("Linked list erased.\n");
+            break;
+        case 7:
+            exit(0);
+        default:
+            printf("Enter correct choice!\n");
+        }
+    }
+    return 0;
+}
 
