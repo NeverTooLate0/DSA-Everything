c04e7a916e5bf066116b1388aa758fd955c5836a
NeverTooLate <azzamxh@gmail.com>
2025-03-29 16:58:47 +0530

    Update insertion_sort.c

--- Arrays/sorting/insertion_sort.c
+++ Arrays/sorting/insertion_sort.c
@@ -14,7 +14,8 @@ void display(int a[], int n) {
 }
 
 void i_sort(int arr[], int n) {  
-    for (int i = 1; i < n; i++) {
+    int i ;
+    for ( i = 1; i < n; i++) {
         int key = arr[i]; 
         int j = i - 1;
 
